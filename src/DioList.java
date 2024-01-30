fae599d8d017316d674793416d014a33b6111254
91112021 <dalebolido@gmail.com>
2024-01-30 13:08:13 -0800

    Added private Node class with methods

--- src/DioList.java
+++ src/DioList.java
@@ -10,5 +10,33 @@ public class DioList
         tail = null;
     }
 
+    private class Node
+    {
+
+        private int data;
+        private Node link;
+
+        public Node (int data, Node link)
+        {
+            this.data = data;
+            this.link = link;
+        }
+
+        public int getData()
+        {
+            return data;
+        }
+
+        public Node getLink()
+        {
+            return link;
+        }
+
+        public String toString()
+        {
+            return "data:" + data + " links to " + link;
+        }
+
+    }
 }
 
