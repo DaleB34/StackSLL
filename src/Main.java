3347c84a0c7d8bd828b64260bb6bfe12d36a871b
91112021 <dalebolido@gmail.com>
2024-02-05 13:14:23 -0800

    Updated Driver

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        DioList stairs = new DioList();
+        stairs.addNode(0);
+        stairs.addNode(1);
+        stairs.addNode(2);
+        stairs.addNode(3);
+
+        stairs.showList();
+        System.out.println("The List Length is: " + stairs.length());
     }
 }
